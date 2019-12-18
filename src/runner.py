1d109314df61e5a27eb0a89b11969913a5e4f565
rizaozcelik <rizaozcelik96@gmail.com>
2019-12-18 10:44:15 +0300

    try 2019

--- src/runner.py
+++ src/runner.py
@@ -1,5 +1,3 @@
-import warnings
-warnings.filterwarnings('ignore')
 import json
 
 from src.ontobiotope import OntoBiotope
@@ -46,18 +44,10 @@ def test(configs):
     test_matchings = extract_mention_node_matchings(configs['dev'])
     X_test, _ = matching_to_embedding(test_matchings, mention_embeddings, node_embeddings)
     Y_test = [node_id for mention, node_id in test_matchings]
-    print(X_test.shape)
     preds = model.predict(X_test, node_embeddings)
     model.evaluate(Y_test, preds, ontobiotope.graph)
-    
-    query = 'children with age less than 5'
-    query_embedding = MentionSet.mention_to_embedding(query, pretrained_word_embeddings)
-    query_embedding = np.array(query_embedding).reshape((1,100))
-    print(query_embedding.shape)
-    p = model.predict(query_embedding, node_embeddings)
-    print(p)
 
-#%%
+
 with open('configs.json') as f:
     configs = json.load(f)
 
@@ -66,38 +56,20 @@ with open(configs['word_embeddings_100']) as embedding_file:
     pretrained_word_embeddings = json.load(embedding_file)
 
 #%%
-node_embeddings = OntoBiotope.load_embeddings(configs['node_embeddings'])
-#%%
-test_matchings = extract_mention_node_matchings(configs['dev'])
-mention_embeddings = MentionSet.load_embeddings(configs['mention_embeddings_100'])
-
-X_test, _ = matching_to_embedding(test_matchings, mention_embeddings, node_embeddings)
-
-#%%
 # train(configs, pretrained_word_embeddings)
 # test(configs)
+
 model = ProjectionModel()
 model.load(configs['model_path'])
 ontobiotope = OntoBiotope(configs['ontobiotope_raw'])
 ontobiotope.load_graph(configs['ontobiotope_nx'])
-
+node_embeddings = OntoBiotope.load_embeddings(configs['node_embeddings'])
+#%%
 finder = Finder()
 finder.construct_inverted_index(configs['train'])
-
-
-#%%
 
 max_distance = 2
 query = 'children with age less than 5'
 query_embedding = MentionSet.mention_to_embedding(query, pretrained_word_embeddings)
-predicted_node_id = model.predict(np.array(query_embedding).reshape((1, 100)), node_embeddings)
-# related_docs = finder.find_related_docs(ontobiotope, predicted_node_id, max_distance)
-#%%
-import networkx as nx
-sps = nx.shortest_path_length(ontobiotope.graph, source=predicted_node_id[0])
-close_nodes = [node for node, dist in sps.items() if dist <= 1]
-
-# related_docs = inverted_index[ontobiotope_id]
-# neighbors = graph.neighbors(ontobiotope_id, 2)
-# neihgbor_docs = [inverted_index[n] for n in neighbors]
-# nei
+predicted_node_id = model.predict(np.array(query_embedding).reshape((1, 100)), node_embeddings)[0]
+related_docs = finder.find_related_docs(ontobiotope.graph, predicted_node_id, max_distance)
